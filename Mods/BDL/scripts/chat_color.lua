34f0650f7c2b243f48ebfa250e6fbee733a9fb0e
Dave G <brightblade@gmail.com>
2023-11-27 13:41:07 -0800

    Add the ability to use a table, enum-type struct to colorize in-game messages

---
+++ Mods/BDL/scripts/chat_color.lua
@@ -0,0 +1,26 @@
+--[[
+MIT License
+
+Copyright (c) 2023 BestDaniel.net
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+--]]
+
+-- This file defines the chat coloring
+return {NONE = "", ORANGE = "<p0>", BLUE = "<p1>", YELLOW = "<p2>", GREEN = "<p3>", PURPLE = "<p4>", BROWN = "<p5>", TEAL = "<p6>", DARKBROWN= "<p7>", ENDCOLOR="</>"}
